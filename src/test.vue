4241d0f63b23b417d42028511129d57cb7028c58
arto <arto@futurewiz.co.kr>
2024-10-29 11:15:54 +0900

    test

--- src/test.vue
+++ src/test.vue
@@ -6,7 +6,7 @@
 import axios from "axios";
 
 const request = async () => {
-  const response = await axios.get("http://localhost:8088/login");
+  const response = await axios.get("https://192.168.3.73:9010/v1/extract_stock/mobile?catchType=best");
   console.log(response)
 }
 </script>
